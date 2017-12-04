8ea0f9175a2e80bf29171abc209493096a493792
adam_polczynski@yahoo.com <adam_polczynski@yahoo.com>
2017-12-04 11:56:36 +0100

    gitignore for settings.py

--- main/models.py
+++ main/models.py
@@ -11,16 +11,7 @@ class Questions(models.Model):
 	wages_4 = models.TextField()
 	wages_5 = models.TextField()
 
-	# def as_dict(self):
-	# 	return {
-	# 	"question": self.id,
-	# 	"wages_1": self.wages_1,
-	# 	"wages_2": self.wages_2,
-	# 	"wages_3": self.wages_3,
-	# 	"wages_4": self.wages_4,
-	# 	"wages_5": self.wages_5
- #            # other stuff
- #            }
+
 	class Meta:
 		db_table = 'questions'
 admin.site.register(Questions)
